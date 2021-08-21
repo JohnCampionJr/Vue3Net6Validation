fa08a38b201c3868af2eb7df7cd0bfa9c6ed2f85
John Campion <john@brightshore.com>
2021-08-21 17:20:58 -0400

    Changed errormessage to not clear errors

--- Result/Result.cs
+++ Result/Result.cs
@@ -7,18 +7,21 @@ public class Result
     public string SuccessMessage { get; set; } = string.Empty;
     public List<string> Errors { get; private set; } = new List<string>();
     public IDictionary<string, string> ValidationErrors { get; set; } = new Dictionary<string, string>();
+    
+    private string errorMessage = "";
     public string ErrorMessage
     {
         get
         {
+            if (!string.IsNullOrEmpty(errorMessage)) return errorMessage;
+
             if (Errors.Count == 0) return string.Empty;
             if (Errors.Count == 1) return Errors[0];
             return string.Join("<br/>", Errors.ToArray());
         }
         set
         {
-            Errors.Clear();
-            Errors.Add(value);
+            errorMessage = value;
         }
     } 
 
